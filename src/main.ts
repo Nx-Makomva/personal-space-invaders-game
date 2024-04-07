4c6c5ce7a69b554610bf6803a721d7aac040d4c6
Nic Makomva <nicholasstm@gmail.com>
2024-04-07 20:48:56 +0200

    animation of hero character complete

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,58 @@ import { Character } from './types'
       - 
 */
 
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
+const context = canvas.getContext('2d')!;
 
+if (!canvas) {
+  throw new Error('Issue with selector');
+}
+
+if (canvas) {
+  canvas.width = canvas.offsetWidth
+  canvas.height = canvas.offsetHeight
+
+} else {
+  console.error('Canvas element not found');
+  
+}
+  
+  const heroImage = [
+    'src/resources/character-sprites/mc-run(1).png',
+    'src/resources/character-sprites/mc-run(2).png',
+    'src/resources/character-sprites/mc-run(3).png',
+  ]
+
+ 
+  let hero: Character = {
+    x: 0,
+    y: 250,
+    width: 100,
+    height: 50,
+    };
+
+
+
+let currentFrameIndex = 0;
+
+const heroImages = heroImage.map((img) => {
+  let imageShowing = new Image();
+  imageShowing.src = img;
+  return imageShowing;
+})
+
+const animateHeroImage = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (context){
+      context.drawImage(heroImages[currentFrameIndex], hero.x, hero.y, hero.width, hero.height)
+    }
+
+      currentFrameIndex = (currentFrameIndex + 1) % heroImages.length;
+      setTimeout(animateHeroImage, 100)
+   
+  }
+
+  animateHeroImage();
 
  
