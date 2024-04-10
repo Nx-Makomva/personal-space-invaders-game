43e5177959256b9b6b9319ccbc35d770fd4fdf79
Nic Makomva <nicholasstm@gmail.com>
2024-04-10 18:55:47 +0200

    npc collision box implemented

--- src/main.ts
+++ src/main.ts
@@ -25,26 +25,25 @@ import { Character } from './types';
 
 /////////////////////////// QUERY SELECTOR /////////////////////////////////
 const heroCharacter = document.querySelector<HTMLDivElement>('#hero');
+const npcCharacter12 = document.querySelector<HTMLDivElement>('.npc-12');
+const collisionBoxElement = document.querySelector<HTMLDivElement>('.npc__collision-box');
 
-const hero: Character = {
-  x: 30, // X-coordinate of the character's position
-  y: 0, // Y-coordinate of the character's position
-  width: 350, // Width of the character's box container
-  height: 50
-}
 /////////////////////////// NULL EXCEPTIONS //////////////////////////////
-if (!heroCharacter){
+if (!heroCharacter || !npcCharacter12 || !collisionBoxElement) {
   throw new Error('Issues with selector');
 }
 
-/////////////////////////// START GAME //////////////////////////////////
-// const handleStartGame = (event: MouseEvent) => {
-//   animateHero();
-// }
-
-// beginGameButton.addEventListener('clcik', handleStartGame);
-
 /////////////////////////// VARIABLES //////////////////////////////////
+const npcCharacterSprites = [
+  'src/resources/character-sprites/npc-0.png',
+  'src/resources/character-sprites/npc-2.png',
+  'src/resources/character-sprites/npc-4.png',
+  'src/resources/character-sprites/npc-6.png',
+  'src/resources/character-sprites/npc-8.png',
+  'src/resources/character-sprites/npc-10.png',
+  'src/resources/character-sprites/npc-12.png',
+]
+
 const heroImagesRun = [
   'src/resources/character-sprites/mc-run(1).png',
   'src/resources/character-sprites/mc-run(2).png',
@@ -54,9 +53,69 @@ const heroImagesRun = [
 const runFrameRate = 10;
 let currentImageIndex = 0;
 let isJumping = false;
-let jumpHeight = 100; // Adjust as needed
-let jumpSpeed = 10; // Adjust as needed
-let gravity = 6; // Adjust as needed
+let jumpHeight = 80; 
+let jumpSpeed = 10; 
+let jumpForwardDash = 4;
+let gravity = 3; 
+let jumpReturnPosition = 0;
+let npcMovementSpeed = 5;
+
+/////////////////////////// CHARACTER OBJECTS //////////////////////////////
+const hero: Character = {
+  x: 0, // X-coordinate of the character's position
+  y: 0, // Y-coordinate of the character's position
+  width: 90, // Width of the character's box container
+  height: 50
+}
+
+const npc12: Character = {
+  x: 300, // X-coordinate of the character's position
+  y: 240, // Y-coordinate of the character's position
+  width: 20, // Width of the character's box container
+  height: 40,
+  img: npcCharacterSprites[6],
+}
+
+const collisionBox = {
+  x: npc12.x,
+  y: npc12.y,
+  width: npc12.width,
+  height: npc12.height,
+  border: {
+    thickness: '1px',
+    style: 'solid',
+    color: 'red',
+  }
+}
+
+collisionBoxElement.style.position = 'absolute';
+collisionBoxElement.style.width = `${collisionBox.width}px`;
+collisionBoxElement.style.height = `${collisionBox.height}px`; 
+collisionBoxElement.style.border = `${collisionBox.border.thickness} ${collisionBox.border.style} ${collisionBox.border.color}`;
+
+
+console.log(collisionBoxElement.style.border);
+
+
+// console.log(collisionBoxElement);
+
+const npcRun = () => {
+  npcCharacter12.style.position = 'absolute';
+  npcCharacter12.style.width = `${npc12.width}px`;
+  npcCharacter12.style.height = `${npc12.height}px`;
+  npcCharacter12.style.left = `${npc12.x}px`;
+  npcCharacter12.style.top = `${npc12.y}px`;
+  npcCharacter12.style.backgroundImage = `url('${npc12.img}')`;
+}
+npcRun();
+
+/////////////////////////// START GAME //////////////////////////////////
+// const handleStartGame = (event: MouseEvent) => {
+//   animateHero();
+// }
+
+// beginGameButton.addEventListener('click', handleStartGame);
+
 
 /////////////////////////// HERO ANIMATION //////////////////////////////////
 
@@ -70,36 +129,44 @@ const animateHero = () => {
 
 animateHero();
 
+// on button press 
+// if statements // on button click switch array // have it all in one function and switch between using default value
+
 /////////////////////////// HERO JUMP HANDLER AND EVENT LISTENER //////////////////////////////////
 
-const handleSpacebarPress = (event: KeyboardEvent) => {
-  console.log(event);
+const handleArrowUp = (event: KeyboardEvent) => {
   
-  if (event.key === ' ' && !isJumping) {
+  if (event.key === 'ArrowUp'&& !isJumping) {
     isJumping = true;
     jump();
   }
 }
     
-document.addEventListener('keydown', handleSpacebarPress);
+document.addEventListener('keydown', handleArrowUp);
 
 
 const fall = () => {
-  if (hero.y < 0) { // Check if the character is above the ground level
-    hero.y += gravity; // Increase the y-position of the character to simulate gravity
+  if (hero.y < 0) { // Check if the hero is above the ground level
+    hero.y += gravity;
+    hero.x += jumpReturnPosition;
     heroCharacter.style.top = `${hero.y}px`;
+    heroCharacter.style.right = `${hero.x}px`;
     setTimeout(fall, 1000 / 60); 
   } else {
     hero.y = 0; 
+    hero.x = 0;
     heroCharacter.style.top = `${hero.y}px`;
-    isJumping = false; // Set isJumping to false when the character reaches the ground
+    heroCharacter.style.right = `${hero.x}px`;
+    isJumping = false; // Set isJumping to false when the hero reaches the ground
   }
 }
 
 const jump = () => {
   if (hero.y >= -jumpHeight) {
     hero.y -= jumpSpeed;
+    hero.x -= jumpForwardDash;
     heroCharacter.style.top = `${hero.y}px`;
+    heroCharacter.style.right = `${hero.x}px`;
     setTimeout(jump, 1000 / 60);
   } else {
     fall();
@@ -107,10 +174,16 @@ const jump = () => {
 }
 
 
+
+// have wider container running whole length of bottom screen
+// to allow other divs to move across it 
+// find way to have the divs x axis to contantly move and if it's 
+// style.left position is equal to the hero's style.right then that's a collision
+
+
   // NEXT STEPS:
- // add event listeners for character jumping - SPACEBAR key press
- // add colleagues to the scene and handle their movement
- // handle collision events with colleagues 
+ // handle collision events with colleagues - use divs and identify collisions 
+ // with x & y positions. Create collision box objects that are equal to the dimensions of characters or less
  // add score counter based on time spent survivng
  // add game over screen and handle restarting game
- // add start screen to initialise game only when player clicks start
+ // add start screen to initialise game only when player clicks start - use classList add & remove to switch between displays being shown or hidden
--- src/types.ts
+++ src/types.ts
@@ -10,5 +10,6 @@ export type Character = {
   x: number; // X-coordinate of the character's position
   y: number; // Y-coordinate of the character's position
   width: number; // Width of the character's box container
-  height: number; // Height of the character's box container
+  height: number; 
+  img?: string// Height of the character's box container
 }
