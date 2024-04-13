c298dd50fd66965e8922be8f32561b4be6399412
Nic Makomva <nicholasstm@gmail.com>
2024-04-14 01:50:20 +0200

    config changes

--- vite-config.js
+++ vite-config.js
@@ -1,17 +1,3 @@
-import { defineConfig } from "vitest/config";
-import { fileURLToPath } from "url";
-export default defineConfig({
-  test: {
-    globals: true,
-  },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        index: fileURLToPath(new URL("index.html", import.meta.url)),
-        game: fileURLToPath(new URL("main.ts", import.meta.url)),
-      },
-    },
-  },
-  base: "./",
-});
+export default { 
+  base: "./" , 
+};
