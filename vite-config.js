32d737e02277a2681116ca340a33a7d209643c66
Nic Makomva <nicholasstm@gmail.com>
2024-04-14 01:30:02 +0200

    updated config settings

--- vite-config.js
+++ vite-config.js
@@ -1,3 +1,17 @@
-export default { 
-  base: "./" , 
-};
+import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+        game: fileURLToPath(new URL("main.ts", import.meta.url)),
+      },
+    },
+  },
+  base: "./",
+});
