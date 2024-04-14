a31e8d2183dc45ff7c5f3a4c90d5ebbd262d48e3
Nic Makomva <nicholasstm@gmail.com>
2024-04-14 14:23:03 +0200

    removed slashes from fle paths

--- src/main.ts
+++ src/main.ts
@@ -80,14 +80,14 @@ let npcMovementSpeed = 25;
 
 /////////////////////////// CHARACTER IMAGES //////////////////////////////
 const npcCharacterSprites = [
-  "./src/resources/character-sprites/npc-0.png",
-  "./src/resources/character-sprites/npc-2.png",
-  "./src/resources/character-sprites/npc-4.png",
-  "./src/resources/character-sprites/npc-6.png",
-  "./src/resources/character-sprites/npc-8.png",
-  "./src/resources/character-sprites/npc-10.png",
-  "./src/resources/character-sprites/npc-12.png",
-  "./src/resources/images/WHAM.png",
+  "src/resources/character-sprites/npc-0.png",
+  "src/resources/character-sprites/npc-2.png",
+  "src/resources/character-sprites/npc-4.png",
+  "src/resources/character-sprites/npc-6.png",
+  "src/resources/character-sprites/npc-8.png",
+  "src/resources/character-sprites/npc-10.png",
+  "src/resources/character-sprites/npc-12.png",
+  "src/resources/images/WHAM.png",
 ];
 
 const heroImagesRun = [
@@ -157,7 +157,7 @@ instructionsButton.addEventListener("click", () => {
 startButton.addEventListener("click", () => {
   startScreen.classList.remove("show-flex");
   if (gameBackground) {
-    gameBackground.style.backgroundImage = `url('/src/resources/environment/office(2).png')`;
+    gameBackground.style.backgroundImage = `url('src/resources/environment/office(2).png')`;
     gameBackground.style.animation = "sceneMovement 9s linear infinite";
   }
 
