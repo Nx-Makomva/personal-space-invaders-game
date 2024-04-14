c3023da48eb4baf4e905e4c32432f362715620d7
Nic Makomva <nicholasstm@gmail.com>
2024-04-15 00:01:50 +0200

    config updates

--- vite-config.js
+++ vite-config.js
@@ -1,3 +1,15 @@
-export default { 
-  base: "./" , 
-};
+import { fileURLToPath } from "url";
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+      },
+    },
+  },
+  base: "./",
+});
